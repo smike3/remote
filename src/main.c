07203311881c845e8a50893885d46c64ab871084
smike3 <smike.gm@gmail.com>
2018-04-18 11:30:04 +0300

    Изменение формата передачи на JSON

--- src/main.c
+++ src/main.c
@@ -29,10 +29,11 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/un.h>
+#include <jansson.h>
 
 #define ADDR_SERV "192.168.0.106"
 
-int command_mvp(char cc[20480],int br)
+/*int command_mvp(char cc[20480],int br)
 {
  DIR *dir;
  int i,fd,rc,ipc_fd;
@@ -48,8 +49,7 @@ int command_mvp(char cc[20480],int br)
              perror("diropen");
              return 0;
          };
-  //   printf("<%s>",cc);
-     //sprintf(cc,"directy");
+  
      while((fname=readdir(dir))!=NULL)
 
              if((fname->d_name[0]!='.')&&(fname->d_type==DT_REG)) {//printf("|%s|",fname->d_name);
@@ -58,7 +58,6 @@ int command_mvp(char cc[20480],int br)
 
 
          closedir(dir);
-//for(i=0;i<br;i++) cc[i]=cc[i+1];
 
  }
  else if(!strncmp(cc,"1mvp",4))
@@ -99,6 +98,109 @@ int command_mvp(char cc[20480],int br)
                close(ipc_fd);
 
 
+         
+
+
+
+            //strcpy(s,"{ \"command\": [\"set\", \"volume\", \"+20\"] }\n");
+            //strcpy(s,"{ \"command\": [\"cycle\", \"pause\"] }\n");
+
+
+        }
+
+
+return strlen(cc);
+}
+*/
+
+json_t * command_mvp(json_t *jcc,int br)
+{
+ DIR *dir;
+ int i,fd,rc,ipc_fd;
+ const char *s,*cc;
+	char ss[20480];
+ struct dirent *fname;
+ struct sockaddr_un ipc;
+	json_t *send_comm,*files,*s_comm;
+	s_comm=json_object();
+	json_object_set(s_comm,"command_type",json_integer(1));
+	send_comm=json_object_get(jcc,"command");
+	cc=json_string_value(send_comm);
+	printf("\n------%s\n",cc);
+ //for(i=0;i<br-4;i++) s[i]=cc[i+4];
+ if(!strncmp(cc,"dir",3)){
+ printf("\n%s\n",s);
+ //cc[4]='\0';
+	 send_comm=json_object_get(jcc,"path");
+	 s=json_string_value(send_comm);
+	 printf("\n%s\n",s);
+     dir=opendir(s);
+     if (!dir) {
+             perror("diropen");
+             return 0;
+         };
+  //   printf("<%s>",cc);
+     //sprintf(cc,"directy");
+	 files=json_array();
+     while((fname=readdir(dir))!=NULL)
+
+             if((fname->d_name[0]!='.')&&(fname->d_type==DT_REG)) {//printf("|%s|",fname->d_name);
+				// files=json_array();
+				 json_array_append(files,json_string(fname->d_name));
+   //          sprintf(cc,"%s!%s",cc,fname->d_name);
+             };
+
+	json_object_set(s_comm,"file_list",files);
+	 json_object_set(s_comm,"command",json_string("dir"));
+         closedir(dir);
+//for(i=0;i<br;i++) cc[i]=cc[i+1];
+
+ }
+ else if(!strncmp(cc,"mvp",3))
+    {
+	//mkfifo("/tmp/clmpv", 0666);
+	 send_comm=json_object_get(jcc,"path");
+	 s=json_string_value(send_comm);
+	 send_comm=json_object_get(jcc,"filename");
+	 strcat(s,json_string_value(send_comm));
+	 json_object_set(send_comm,"command",json_string("mpv"));
+	 printf("\n%s\n",s);
+       switch(fork())
+	{
+	case -1:
+	    perror("Fork faild");
+	    exit(1);
+	case 0:
+	    execl("/usr/bin/mpv", "/usr/bin/mpv","--input-ipc-server=/tmp/clmpv","--vo","opengl",s,NULL);
+	    
+	    exit(0);
+	}
+
+    }
+    else {
+            ipc_fd=socket(AF_UNIX, SOCK_STREAM, 0);
+            ipc.sun_family = AF_UNIX;
+             strcpy(ipc.sun_path,"/tmp/clmpv");
+             rc = connect(ipc_fd, (struct sockaddr *) &ipc, sizeof(struct sockaddr_un));
+		printf("\n------%s\n",cc);
+            if(!strncmp(cc,"stp",3)) {strcpy(ss,"{ \"command\": [\"osd-msg-bar\", \"cycle\", \"pause\"] }\n"); printf("\n------%s\n",ss);} 
+            else if(!strncmp(cc,"vup",3)) strcpy(ss,"{ \"command\": [\"osd-msg-bar\", \"add\", \"volume\", \"+5\"] }\n");
+                else if(!strncmp(cc,"vdn",3)) strcpy(ss,"{ \"command\": [\"osd-msg-bar\", \"add\", \"volume\", \"-5\"] }\n");
+                    else if(!strncmp(cc,"qui",3)) strcpy(ss,"{ \"command\": [\"quit\", \"1\"] }\n");
+                	else if(!strncmp(cc,"adv",3)) strcpy(ss,"{ \"command\": [\"osd-msg-bar\", \"seek\",\"+5\"] }\n");
+                	    else if(!strncmp(cc,"rev",3)) strcpy(ss,"{ \"command\": [\"osd-msg-bar\", \"seek\",\"-5\"] }\n");
+                		else if(!strncmp(cc,"aad",3)) strcpy(ss,"{ \"command\": [\"osd-msg-bar\", \"seek\",\"+300\"] }\n");
+                		    else if(!strncmp(cc,"rre",3)) strcpy(ss,"{ \"command\": [\"osd-msg-bar\", \"seek\",\"-300\"] }\n");
+                rc = write(ipc_fd, ss, strlen(ss));
+                printf("\n@@@@@@%s",ss);
+                rc = read(ipc_fd, ss, 20480);
+				json_object_set(s_comm,"command",json_string("okk"));
+                //    write(fd,"{ \"command\": [ \"seek\", \"60\"] }",31);
+             //   	write(fd,"{ \"command\": [\"set_property_string\", \"pause\",\"yes\"] }",53);
+             //  printf("\n{ \"command\": [ \"seek\", \"60\"] }\n %d|%s",rc,s);
+               close(ipc_fd);
+
+
             /*if (ipc_fd < 0) {
                 MP_ERR(arg, "Could not create IPC socket\n");
                 goto done;
@@ -113,16 +215,25 @@ int command_mvp(char cc[20480],int br)
         }
 
 
-return strlen(cc);
+	
+	//json_pack("{s:i}","volume",1);
+return s_comm;
 }
 
-int command_aud(char cc[20480],int br)
+void send_cover(char *fl_path)
+{
+	printf("\n%s\n",fl_path);
+	return;
+}
+
+/*int command_aud(char cc[20480],int br)
 {
     pid_t p;
-    int cpipe[2],rbb,i,rb,fd;
+    int cpipe[2],rbb,i,rb,fd,send_cover_fl=0;
     pipe(cpipe);
 //    int rb[2];
-    char bb[256]="",c1[4],c2[2048],*c;
+    char bb[256]="",c1[4],c2[2048],c3[2048],*c;
+	
     for(i=0;i<br;i++) cc[i]=cc[i+1];
     switch(fork())
 	{
@@ -134,7 +245,8 @@ int command_aud(char cc[20480],int br)
 	    dup2(cpipe[1],1);
 	    br--;
 	    if(cc[br]=='\n') cc[br]='\0';
-	    if(!strncmp(cc,"current-song",12)) execl("/usr/bin/audtool","/usr/bin/audtool","get-volume",cc,NULL);
+		
+	    if(!strncmp(cc,"current-song",12)) execl("/usr/bin/audtool","/usr/bin/audtool","get-volume",cc,"current-song-filename",NULL);
 	    if(!strncmp(cc,"set-volume",10)){
 		c=strchr(cc,' ');
 		c++;
@@ -145,36 +257,108 @@ int command_aud(char cc[20480],int br)
 	    close(cpipe[1]);
 	    exit(0);
 	}
-/*    close(cpipe[1]);
-    rb[0]=read(cpipe[0],cc,256);
-    cc[--rb[0]]='\0';
-    rb[1]=atoi(cc);
-    rb[0]=read(cpipe[0],cc,256);
-    close(cpipe[0]);
-    cc[--rb[0]]='\0';
-    printf("{%s}(%d)[%d]",cc,rb[0],rb[1]);*/
+
     close(cpipe[1]);
     rb=read(cpipe[0],c1,5);
     c1[--rb]='\0';
     rb=read(cpipe[0],c2,2048);
+	c2[--rb]='\0';
+	rb=read(cpipe[0],c3,2048);
+	c3[--rb]='\0';
     close(cpipe[0]);
-    c2[--rb]='\0';
+    
+	printf("\n##%s###%s###\n",c1,c2);
     if(rb<0)
     {
     sprintf(cc,"0[%03d]none",atoi(c1));
     rb=4;
     }
-    else sprintf(cc,"0[%03d]%s",atoi(c1),c2);
+    else sprintf(cc,"0[%03d]%s#%s",atoi(c1),c2,c3);
     printf("{%s}(%d}",cc,rb);
     return rb+6;
+}
+*/
+
+json_t * command_aud(json_t *jcc,int br)
+{
+    pid_t p;
+    int cpipe[2],rbb,i,rb,fd,send_cover_fl=0,v;
+    pipe(cpipe);
+	json_t *send_comm;
+	const char *cc;
+  //  int rb[2];
+    char bb[256]="",c1[4],c2[2048],c3[2048],*c;
+	
+  //  for(i=0;i<br;i++) cc[i]=cc[i+1];
+	send_comm=json_object_get(jcc,"command");
+	cc=json_string_value(send_comm);
+	printf("aud activate - %s\n",cc);
+    switch(fork())
+	{
+	case -1:
+	    perror("Fork faild");
+	    exit(1);
+	case 0:
+	    close(cpipe[0]);
+	    dup2(cpipe[1],1);
+		//	printf("fork\n");
+	  //  br--;
+	   // if(cc[br]=='\n') cc[br]='\0';
+		/*if(!strncmp(cc,"view-cover",10))
+		
+			{
+		//	send_cover_fl=1;
+		//	printf("#########");
+			execl("/usr/bin/audtool","/usr/bin/audtool","current-song-filename",cc,NULL);
+			}*/
+	    if(!strncmp(cc,"current-song",12)) 
+		{
+		//	printf("current-song activate\n");
+				execl("/usr/bin/audtool","/usr/bin/audtool","get-volume",cc,"current-song-filename",NULL);
+		}
+	    if(!strncmp(cc,"set-volume",10)){
+		c=strchr(cc,' ');
+		c++;
+		send_comm=json_object_get(jcc,"volume");
+		v=json_integer_value(send_comm);
+			sscanf(&c,"%d",v);
+	     execl("/usr/bin/audtool","/usr/bin/audtool","set-volume",c,"get-volume","current-song",NULL);
+	     }
+	    else execl("/usr/bin/audtool","/usr/bin/audtool",cc,"get-volume","current-song","current-song-filename",NULL);
+	    printf("\n[%s]\n",cc);
+	    close(cpipe[1]);
+	    exit(0);
+	}
+    close(cpipe[1]);
+    rb=read(cpipe[0],c1,5);
+    c1[--rb]='\0';
+    rb=read(cpipe[0],c2,2048);
+	c2[--rb]='\0';
+	rb=read(cpipe[0],c3,2048);
+	c3[--rb]='\0';
+    close(cpipe[0]);
+    send_comm=json_pack("{s:i,s:s,s:s,s:i}","volume",atoi(c1),"title",c2,"path",c3,"command_type",0);
+	printf("\n!!!%s!!!%s!!!%s\n",c1,c2,c3);
+    if(rb<0)
+    {
+    sprintf(cc,"0[%03d]none",atoi(c1));
+    rb=4;
+    }
+    else sprintf(cc,"0[%03d]%s#%s",atoi(c1),c2,c3);
+    printf("{%s}(%d}",cc,rb);
+	printf("\n%s\n",json_dumps(send_comm,0));
+   // return rb+6;
+	return send_comm;
 }
 
 int main ()
 {
-    int sc, ls, c,i;       // дескрипторы сокетов
+    int sc, ls, c=0,i,com_t;       // дескрипторы сокетов
     struct sockaddr_in addr; // структура с адресом
-    char buf[20480];       // буфур для приема
+    char buf[20480],*com;       // буфур для приема
     int bread;           // кол-во принятых байт
+	json_t *comm,*type,*scomm,*comm_t;
+	json_error_t er;
     ls=socket(AF_INET,SOCK_STREAM,0); // создаем сокет для входных подключений
     if(ls<0)
 	{
@@ -215,14 +399,20 @@ int main ()
 	             if(bread<=0) break;
     	    	     printf("Получено %d bytes\tСообщение: %s\n",bread,buf);
         	     printf("Отправляю принятое сообщение клиенту\n\n\n");
-        	     printf("!%c!\n",buf[0]);
-        	     switch(buf[0])
+        	   //  printf("!%c!\n",buf[0]);
+				comm=json_loads(buf,0,&er);
+				comm_t=json_object_get(comm,"command_type");
+				if(comm_t) com_t=json_integer_value(comm_t);
+				//json_unpack(comm,"{s:s,s:i}","command",&com,"command_type",&com_t);
+				printf("\n#%d#%s#\n",com_t,com);
+        	  //   switch(buf[0])
+				switch(com_t)
         	     {
 //        	     case '0': c=command_aud(buf,bread,c);
-        	     case '0': c=command_aud(buf,bread);
+        	     case 0: scomm=command_aud(comm,bread);
         	        break;
 //        	     case '1': c=command_mvp(buf,bread,c);
-        	     case '1': c=command_mvp(buf,bread);
+        	     case 1: scomm=command_mvp(comm,bread);
         	        break;
         	     default: buf[0]='\0';
 
@@ -231,9 +421,13 @@ int main ()
 
         //	     read(fileno(stdout),buf,2048);
 			//if(rb==-1) send(sc,buf,c,0);
-    		     printf("C: %d<%s>\n",c,buf);
-	    	     if(c>4) send(sc,buf,c,0); // отправляем принятое сообщение клиенту
-	    	     else send(sc,"0[000]none",10,0);
+    		     printf("C: %d<%s><%s>\n",c,buf,com);
+
+				printf("\n%d - %s\n",json_dumpb(scomm,NULL,0,0),json_dumps(scomm,0));
+	    	 //    if(c>4) send(sc,buf,c,0); // отправляем принятое сообщение клиенту
+				//if(c>4) send(sc,json_dumps(scomm,0),json_dumpb(scomm,NULL,0,0),0); // отправляем принятое сообщение клиенту
+	    	     //else send(sc,"0[000]none",10,0);
+			send(sc,json_dumps(scomm,0),json_dumpb(scomm,NULL,0,0),0);
     		    }
     	        close(sc); // закрываем сокет
     	        exit(0);
